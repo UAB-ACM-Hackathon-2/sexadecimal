f7e4a08384ecda36d5aecb066c6accb2f6c832cd
jamall harris <jharri34@uab.edu>
2015-01-31 22:54:54 -0600

    just added music

---
+++ hackathon2015/src/js/states/Boot.js
@@ -0,0 +1,25 @@
+/* global GameCtrl */
+(function () {
+    'use strict';
+
+    GameCtrl.Boot = function () {};
+
+    GameCtrl.Boot.prototype = {
+
+        preload: function () {
+
+            this.load.image('preloaderBackground', 'assets/images/progress_bar_background.png');
+            this.load.image('preloaderBar', 'assets/images/progress_bar.png');
+            this.load.audio('epicmusic', ['assets/audio/epicmusic.mp3', 'assets/audio/epicmusic.ogg']);
+
+        },
+
+        create: function () {
+            var music;
+            music = game.add.audio('epicmusic');
+            music.play();
+            this.game.state.start('Preloader');
+        }
+    };
+
+})();
